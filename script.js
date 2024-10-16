1de87bc2d0854d02cdd685e430d529bb4506a747
Gaurav Prajapat <Gauravprajapat2305@gmail.com>
2024-10-16 11:29:32 +0530

    computer play added

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-let turn = 'X';  // Start with 'X'
+let turn = 'X'; 
 let gameover = false;
 let X = 0;
 let O = 0;
-let mode = 'u-vs-u'; // Default mode
+let mode = 'u-vs-u'; 
 
-// Get selected game mode
 document.getElementById('gameMode').addEventListener('change', function() {
   mode = this.value;
 });
 
-// Function to switch turns
 let changeTurn = () => {
   return turn === 'X' ? '0' : 'X';
 }
 
-// Function to check for a win or a draw
 let checkWin = () => {
   let boxtext = document.getElementsByClassName('boxtext');
   let win = [
@@ -28,7 +25,6 @@ let checkWin = () => {
     [2, 4, 6, 0, 17, 135, 630, 0, -6]
   ];
 
-  // Check for winning conditions
   win.forEach(e => {
     if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && 
         (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && 
@@ -42,7 +38,7 @@ let checkWin = () => {
       }
       gameover = true;
 
-      // Adjust the line for the winning condition
+      
       line.style.transform = `translate(${e[3]}vw , ${e[4]}vw) rotate(${e[5]}deg)`;
       line.style.width = `${e[6]}px`;
       line.style.left = `${e[7]}px`;
@@ -52,17 +48,14 @@ let checkWin = () => {
     }
   });
 
-  // Check for draw: If all boxes are filled and there's no winner
   let isDraw = Array.from(boxtext).every(box => box.innerText !== '');
   if (isDraw && !gameover) {
     document.querySelector('.turn').innerText = 'It\'s a Draw!';
     gameover = true;
-    document.querySelector('.hidden').style.display = 'block'; // Optionally show a message
-    // No points awarded for a draw
+    document.querySelector('.hidden').style.display = 'block'; 
   }
 }
 
-// Function for the computer's move
 let computerMove = () => {
   let boxes = document.getElementsByClassName('boxes');
   let emptyBoxes = Array.from(boxes).filter(element => {
@@ -83,9 +76,27 @@ let computerMove = () => {
   }
 }
 
-// Reset button to clear the game
+let boxes = document.getElementsByClassName('boxes');
+Array.from(boxes).forEach(element => {
+  element.addEventListener('click', () => {
+    let boxtext = element.querySelector('.boxtext');
+    if (boxtext.innerText === '' && !gameover) {
+      boxtext.innerText = turn;
+      checkWin();
+      if (!gameover) {
+        turn = changeTurn();
+        document.getElementsByClassName('turn')[0].innerText = 'Turn for ' + turn;
+
+        if (mode === 'computer-vs-u' && turn === '0') {
+          computerMove();
+        }
+      }
+    }
+  });
+});
+
 let reset = document.querySelector('.reset');
-reset.addEventListener('click', () => {
+let resetGame =  () => {
   let line = document.querySelector('.line');
   let boxtext = document.querySelectorAll('.boxtext');
   boxtext.forEach(element => {
@@ -98,34 +109,15 @@ reset.addEventListener('click', () => {
   line.style.left = `0px`;
   line.style.top = `0px`;
   document.querySelector('.hidden').style.display = 'none';
-});
+}
+reset.addEventListener('click', resetGame );
 
-// Add click event to boxes
-let boxes = document.getElementsByClassName('boxes');
-Array.from(boxes).forEach(element => {
-  element.addEventListener('click', () => {
-    let boxtext = element.querySelector('.boxtext');
-    if (boxtext.innerText === '' && !gameover) {
-      boxtext.innerText = turn;
-      checkWin();
-      if (!gameover) {
-        turn = changeTurn();
-        document.getElementsByClassName('turn')[0].innerText = 'Turn for ' + turn;
 
-        // If the mode is computer vs user, let the computer move
-        if (mode === 'computer-vs-u' && turn === '0') {
-          computerMove();
-        }
-      }
-    }
-  });
-});
-
-// Reset score button
 document.getElementById('reset').addEventListener('click', () => {
   X = 0;
   O = 0;
   document.querySelector('.x').innerText = 'X = 0';
   document.querySelector('.o').innerText = 'O = 0';
   resetGame();
+ 
 });
